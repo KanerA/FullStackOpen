ce053a3611b2f2439337fd6f414f527c56ca176c
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-07 19:15:47 +0200

    create phonebook

--- index.js
+++ index.js
@@ -3,3 +3,26 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+
+const phonebook = [
+    {
+        id: 1,
+        name: 'Assaf',
+        number: '050-4376818'
+    },
+    {
+        id: 2,
+        name: 'Ora',
+        number: '050-5317886'
+    },
+    {
+        id: 3,
+        name: 'Yoav',
+        number: '050-3731688'
+    },
+    {
+        id: 4,
+        name: 'Hanan',
+        number: '054-2585082'
+    }
+]
