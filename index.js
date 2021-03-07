cdcf738b88f39c7da9441299d806bd0d49461138
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-07 19:19:46 +0200

    finish ex. 3.2
    send how much contacts are in the phonebook and time of the request.

--- index.js
+++ index.js
@@ -31,6 +31,10 @@ app.get('/api/persons', (req, res) => {
     res.json(phonebook);
 });
 
+app.get('/info', (req, res) => {
+    res.send(`<p>PhoneBook has ${phonebook.length} contacts in it</p><p>${new Date()}</p>`)
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`app is running on port ${PORT}`);
