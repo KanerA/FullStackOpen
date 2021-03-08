9acdf2e36328be73cd75a03521f6d13e32ebf00a
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-08 14:02:39 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -69,7 +69,7 @@ app.post('/api/persons', (req, res) => {
     res.json({body: body, message: "success"})
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`app is running on port ${PORT}`);
 });
