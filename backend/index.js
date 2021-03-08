29d36400ac0bd732951d0c2fcacdfa697febcd29
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-08 13:58:25 +0200

    install cors

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,13 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const cors = require('cors');
 
 morgan.token('req-body', (req, res) => {
     return JSON.stringify(req.body);
 })
 
+app.use(cors())
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :req-body'));
 
