54d2f7af7a2bec7be1acb5d3962ee0dd38c62616
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-09 21:06:52 +0200

    Update the Person module

--- models/person.js
+++ models/person.js
@@ -3,10 +3,17 @@ const mongoose = require('mongoose');
 const url = process.env.MONGODB_URI;
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+.then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({ // defines how a person object should look
   name: String,
   number: String,
+  id: Number,
 })
 
 personSchema.set('toJSON', {
