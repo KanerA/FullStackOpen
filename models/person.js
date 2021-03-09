fb5ecdce3b462673ea68fd4f69a78f25f3e984c9
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-09 19:23:42 +0200

    Create a Person module

---
+++ models/person.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const url = process.env.MONGODB_URI;
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({ // defines how a person object should look
+  name: String,
+  number: String,
+})
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
+
+const Person = mongoose.model('Person', personSchema)
+
+module.exports = Person;
