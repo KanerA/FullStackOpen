1dd8a7050af1bbcaed3cf342abf521e5f75a0473
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-09 21:10:39 +0200

    answer ex. 3.13 & 3.14
    all get is from DB and post is to DB

--- backend/index.js
+++ backend/index.js
@@ -16,29 +16,6 @@ app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :req-body'));
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')))
 
-const phonebook = [
-    {
-        id: 1,
-        name: 'Assaf',
-        number: '050-4376818'
-    },
-    {
-        id: 2,
-        name: 'Ora',
-        number: '050-5317886'
-    },
-    {
-        id: 3,
-        name: 'Yoav',
-        number: '050-3731688'
-    },
-    {
-        id: 4,
-        name: 'Hanan',
-        number: '054-2585082'
-    }
-]
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
 })
@@ -50,14 +27,16 @@ app.get('/api/persons', (req, res) => {
 });
 
 app.get('/info', (req, res) => {
-    res.send(`<p>PhoneBook has ${phonebook.length} contacts in it</p><p>${new Date()}</p>`)
+    Person.find({})
+        .then(people => {
+            res.send(`<p>PhoneBook has ${people.length} contacts in it</p><p>${new Date()}</p>`)
+        })
 });
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const person = phonebook.find(p => p.id === id);
-    if(!person) return res.status(404).send('<h1>ID Not Found</h1>')
-    res.json(phonebook[id-1]);
+    Person.findById(req.params.id).then(person => {
+        res.json(person)
+      })
 });
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -70,13 +49,19 @@ app.delete('/api/persons/:id', (req, res) => {
 app.post('/api/persons', (req, res) => {
     const { body } = req;
 
-    if(!body.name || !body.number) return res.status(400).json({message: "can't add a contact without a name or number"});
-    const person = phonebook.find(p => p.name === body.name);
-    if(person) return res.status(200).json({ error: 'name must be unique' })
+    if(body.number === undefined){
+        return res.status(400).json({ error: 'number is missing' });
+    }
     
-    body.id = Math.floor(Math.random() * 1000);
-    phonebook.push(body);
-    res.json({body: body, message: "success"})
+    const person = new Person({
+        name: body.name,
+        number: body.number,
+        id: Math.floor(Math.random() * 1000),
+    })
+
+    person.save().then(savedPerson => {
+        res.json(savedPerson);
+    })
 });
 
 const PORT = process.env.PORT;
